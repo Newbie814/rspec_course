648cf8ea8d5c4b6556c514d745c2fa433057a64f
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-03 14:53:06 -0400

    update

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,5 @@
 class Card
-  attr_reader :rank, :suit
+  attr_accessor :rank, :suit
 
   def initialize(rank, suit)
     @rank = rank
@@ -8,17 +8,20 @@ class Card
 end
 
 RSpec.describe Card do
-  def card 
-     Card.new('Ace', 'Spades')
-  end
+  let(:card) { Card.new('Ace', 'Spades') }
   
   it 'has a rank' do
-      
     expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
+  end
+  
+  it 'has a suit' do
+    expect(card.suit).to eq('Spades')
   end
 
-  it 'has a suit' do
-      
-    expect(card.suit).to eq('Spades')
+  it 'has a custom error message' do
+    comparison = 'Spade'
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead!"
   end
 end
