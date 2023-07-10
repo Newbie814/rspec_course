b39ff37aff399552c83dae089a4d55b2a1849383
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-10 12:30:42 -0400

    raise error and respond to

--- spec/raise_error_matcher_spec.rb
+++ spec/raise_error_matcher_spec.rb
@@ -6,9 +6,14 @@ RSpec.describe 'raise_error matcher' do
   # it 'can check for any error' do
   #   expect { some_method }.to raise_error
   # end
+  class CustomError < StandardError; end
 
   it 'can check for a specific error being raised' do
     expect { some_method }.to raise_error(NameError)
     expect { 10 / 0 }.to raise_error(ZeroDivisionError)
+  end
+
+  it 'can check for user-created error' do
+    expect { raise CustomError }.to raise_error(CustomError)
   end
 end
--- spec/respond_to_matcher_spec.rb
+++ spec/respond_to_matcher_spec.rb
@@ -1,5 +1,27 @@
-RSpec.describe 'respond_to matcher' do
-  it 'checks for object attribute existence' do
-    
+class HotChocolate
+  def drink
+    'Delicious'
+  end
+
+  def discard
+    'PLOP!'
+  end
+
+  def purchase(number)
+    "Awesome, I just purchased #{number} more hot chocolate beverages!"
   end
 end
+
+RSpec.describe HotChocolate do
+  it 'confirms that an object can respond to a method' do
+    expect(subject).to respond_to(:drink)
+    expect(subject).to respond_to(:drink, :discard, :purchase)
+    
+  end
+
+  it 'confirms an object can respond to a method with arguments' do
+    # expect(subject).to respond_to(:purchase)
+    expect(subject).to respond_to(:purchase).with(1).arguments
+  end
+end
+
