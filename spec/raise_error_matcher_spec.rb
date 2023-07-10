3f9084928f34a524a8a24bea331604a7c7958593
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-07-09 21:13:28 -0400

    include and raise error

--- spec/raise_error_matcher_spec.rb
+++ spec/raise_error_matcher_spec.rb
@@ -1,5 +1,14 @@
 RSpec.describe 'raise_error matcher' do
-  it 'checks for a specific error type' do
-    
+ def some_method
+  x
+ end
+
+  # it 'can check for any error' do
+  #   expect { some_method }.to raise_error
+  # end
+
+  it 'can check for a specific error being raised' do
+    expect { some_method }.to raise_error(NameError)
+    expect { 10 / 0 }.to raise_error(ZeroDivisionError)
   end
 end
